ef2be98f2820aa6d182269a1d150f43b830604d5
Luccas <manfrediluccas07@yahoo.com>
2026-09-17 09:08:49 -0300

    Atividade de javasctipt

---
+++ Atividade-1/exercicio1.js
@@ -0,0 +1,5 @@
+const entrada = require ('readline=sync');
+
+const qtdPorHora = entrada.questionInt("Digite a quantidade de pecas produzidas por hora: ")
+const horasTurno = entrada.questionInt("Digite as horas trabalhadas por turno: ")
+
